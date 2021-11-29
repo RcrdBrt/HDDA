b82756d8bbc2917429c5e506322a8f74322b1072
Alessandro Venturi <a.venturi2@campus.unimib.it>
2021-11-29 21:15:19 +0100

    Survival updated, --- don't run line 676---

--- data_preparation/pie_chart_default_VS_fully_paid.R
+++ data_preparation/pie_chart_default_VS_fully_paid.R
@@ -8,7 +8,6 @@ piechart <- function(d, group, legend_title = "Outcome"){
     geom_bar(width = 1, stat = "identity", color = "white") +
     coord_polar("y", start = 0) +
     geom_text(aes(label = n_perc), position = position_stack(vjust = 0.5),color = "white")+
-    scale_fill_brewer("Blues") +
     guides(fill=guide_legend(title=legend_title))+
     theme_void()
 }
