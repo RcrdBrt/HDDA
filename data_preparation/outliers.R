fc53b551bd1c4cafe1402de20b322d8471f55e89
Riccardo Berto <riccardobrt@gmail.com>
2021-12-08 17:29:07 +0100

    bump

--- data_preparation/outliers.R
+++ data_preparation/outliers.R
@@ -1,11 +1,11 @@
-FIND_EXTREME_OBSERVARION <- function(x,sd_factor=3){
+FIND_EXTREME_OBSERVATION <- function(x,sd_factor=3){
   which(x>mean(x)+sd_factor*sd(x) | x<mean(x)-sd_factor*sd(x))
 }
 
 out_idxs <- c()
 
 for (i in 1:length(numeric_variables)){
-  out_idxs <- c(out_idxs, FIND_EXTREME_OBSERVARION(acc[,numeric_variables[i]]))
+  out_idxs <- c(out_idxs, FIND_EXTREME_OBSERVATION(acc[,numeric_variables[i]]))
 }
 
 out_idxs <- unique(out_idxs)
